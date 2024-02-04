c2e5da7e35b359d83f89b768c9c8d7e72e1c962b
Syed Hussain <syed.hussain@eax360.com>
2024-02-04 08:50:42 +0000

    Added API.REST.POST

---
+++ API.REST.POST/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Text.Json;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var uri = "https://httpbin.org/post";
+        var dataObject = new { Name = "John Doe", Age = 30 };
+
+        string json = JsonSerializer.Serialize(dataObject);
+        await PostJsonAsync(uri, json);
+    }
+
+    public static async Task PostJsonAsync(string uri, string json)
+    {
+        using (var client = new HttpClient())
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, uri)
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+
+            requestMessage.Headers.Add("X-Custom-Header", "YourValueHere");
+
+            var response = await client.SendAsync(requestMessage);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody);
+            }
+            else
+            {
+                Console.WriteLine($"Failed to POST data: {response.StatusCode}");
+            }
+        }
+    }
+}
